09dd246cff4f283d40755f3759ce72a1a3b932fb
haitrieu123580 <haitrieu123580@gmail.com>
2023-05-19 21:32:16 +0700

    add and remove order_item

--- core/models.py
+++ core/models.py
@@ -33,15 +33,21 @@ class Item(models.Model):
         })
 
     def get_add_to_cart(self):
-        return  reverse("core:add-to-cart", kwargs={
+        return reverse("core:add-to-cart", kwargs={
+            'slug': self.slug
+        })
+
+    def get_remove_from_cart(self):
+        return reverse("core:remove-from-cart", kwargs={
             'slug': self.slug
         })
 
 
 class OrderItem(models.Model):
-
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
+    ordered = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.quantity} of {self.item.title}'
--- core/views.py
+++ core/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Item, OrderItem, Order
 from django.views.generic import ListView, DetailView
+
+from django.contrib.auth.decorators import login_required
 from django.utils import timezone
+from django.contrib import messages
 # Create your views here.
 
 
@@ -33,20 +36,51 @@ def checkout(request):
     return render(request, 'checkout.html')
 
 
-# function to handler adding to cart
+# adding/removing orderItem
+@login_required
 def add_to_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
-    order_item = OrderItem.objects.create(item = item)
+    # find or create new order item belong to user's order
+    order_item, created = OrderItem.objects.get_or_create(
+        item=item,
+        user=request.user,
+        ordered=False
+    )
+    # find the order exist
     order_qs = Order.objects.filter(user=request.user, ordered=False)
     if order_qs.exists():
         order = order_qs[0]
         # check if the order item is in the order
-        if order.items.filter(item__slug=item.slug).exist():
-            order_item.quantity+=1
-            order_item.save
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+            messages.info(request, 'This item quantity was updated :>')
         else:
-            ordered_date = timezone.now()
-            order = Order.objects.create(user = request.user, ordered_date = ordered_date)
             order.items.add(order_item)
-
+            messages.info(request, 'This item was added to your cart :>')
+    else:
+        order = Order.objects.create(user=request.user, ordered_date=timezone.now())
+        order.items.add(order_item)
+        messages.info(request, 'This item was added to your cart :>')
     return redirect("core:product", slug=slug)
+
+@login_required
+def remove_from_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    # find the order exist
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = OrderItem.objects.filter(item=item, user=request.user, ordered=False)[0]
+            order.items.remove(order_item)
+            order_item.delete()
+            messages.info(request, 'This item was remove from your cart:>')
+        else:
+            messages.info(request, 'This item was not in your cart :>')
+    else:
+        messages.info(request, 'You do not have an active order :>')
+    return redirect("core:product", slug=slug)
+
+
